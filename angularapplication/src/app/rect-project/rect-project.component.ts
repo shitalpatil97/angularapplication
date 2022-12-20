35f0626f4bb309e9c7ce4d1868c09543789dc363
pravinp3536 <shitalpatilpawar97@gmail.com>
2022-12-20 17:49:49 +0530

    reactive form assignment on dated 20 dec
    reactive form assignment on dated 20 dec

--- angularapplication/src/app/rect-project/rect-project.component.ts
+++ angularapplication/src/app/rect-project/rect-project.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-rect-project',
@@ -6,10 +8,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rect-project.component.css']
 })
 export class RectProjectComponent implements OnInit {
+  myForm:FormGroup;
+  submit:boolean=false;
 
-  constructor() { }
+  constructor() {
+   this.createForm();
+
+   }
+   onSubmit(){
+    console.log(`Project Name is: ${this.myForm.controls.project.value}`);
+    console.log(`Email ID is: ${this.myForm.controls.email.value}`);
+    console.log(`Project Status is: ${this.myForm.controls.status.value}`);
+
+
+    this.submit=true;
+
+
+   }
+  
+noNames(control:FormControl): Promise<any> | Observable<any> {
+  const myResponse = new Promise<any>((resolve, reject) => {
+    setTimeout(() => {
+      if(control.value === 'Test'){
+        resolve({'emailNotAllowed': true})
+      } else {
+        resolve(null)
+      }
+    }, 1000);
+  })
+  return myResponse;
+}
+   createForm(){
+    this.myForm=new FormGroup({
+      'project':new FormControl('',[Validators.required],this.noNames),
+      'email':new FormControl('',[Validators.required,Validators.email]),
+      'status':new FormControl('Finished',Validators.required)
+
+    })
+
+   }
 
   ngOnInit() {
+    
   }
 
 }
